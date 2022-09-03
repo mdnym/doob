7a7d23d3281f6f9e0f63fb61d9ad401d2fb8c5c6
mdnym <60385356+mdnym@users.noreply.github.com>
2022-09-03 13:53:28 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,7 +69,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 500, 'easeInOutExpo');
+        }, 300, 'easeInOutExpo');
         event.preventDefault();
     });
 });
